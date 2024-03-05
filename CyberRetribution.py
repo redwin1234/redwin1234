58d588e19c7cfb41caf77057a5fd1e67c3117d79
redwin1234 <162277088+redwin1234@users.noreply.github.com>
2024-03-05 11:04:03 +0530

    Add files via upload

---
+++ CyberRetribution.py
@@ -0,0 +1,50 @@
+print("Cyber Retribution")
+name = input("You are name:");
+age =int(input("You are age:"));
+print(name,"OK")
+print(age,"OK")
+
+
+
+	
+print("You are name");
+
+
+	
+
+a =int(input("Enter:"));
+
+b=int(input("Enter:"));
+
+x=a+b;
+
+
+
+m=a/b;
+
+
+
+d=a-b;
+
+
+
+v=a*b;
+
+
+print("=",x)
+
+
+
+print("=",m)
+
+
+
+print("=",d)
+
+
+
+
+print("=",v)
+
+
+print("OK")
